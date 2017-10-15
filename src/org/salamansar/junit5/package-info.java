9352f617e13a5612a65175a5a079fcfb3d25fe51
salamansar <salamansar@gmail.com>
2017-10-15 13:34:02 +0300

    Initial component structure
    Initial component structure. This commit adds new template for simplest JUnit5 test class.

---
+++ src/org/salamansar/junit5/package-info.java
@@ -0,0 +1,13 @@
+@TemplateRegistration(
+        folder = "Other",
+        iconBase="org/salamansar/junit5/junit5.gif", 
+        displayName = "#Junit5template_displayName", 
+        content = "JUnit5Test.java.template",
+        description = "Description.html",
+        scriptEngine="freemarker"
+)
+@Messages(value = "Junit5template_displayName=JUnit5 Test Calss")    
+package org.salamansar.junit5;
+
+import org.netbeans.api.templates.TemplateRegistration;
+import org.openide.util.NbBundle.Messages;
